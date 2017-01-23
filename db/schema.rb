4242984d84119f625120dbd0259f8352656c7a9b
samskinnerphd <samskinnerphd@gmail.com>
2017-01-22 19:04:45 -0500

    add SalesFigues model

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+ActiveRecord::Schema.define(version: 20170122235104) do
+  enable_extension "plpgsql"
+
+  create_table "sales_figures", force: :cascade do |t|
+    t.integer  "year"
+    t.string   "geo_code"
+    t.string   "jurisdiction"
+    t.string   "zipcode"
+    t.integer  "total_sales"
+    t.float    "median_value"
+    t.float    "mean_value"
+    t.integer  "sales_inside_pfa"
+    t.float    "median_value_in_pfa"
+    t.float    "mean_value_in_pfa"
+    t.integer  "sales_outside_pfa"
+    t.float    "median_value_out_pfa"
+    t.float    "mean_value_out_pfa"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+end
