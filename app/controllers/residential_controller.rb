2b9c0c3523625a858a5194b2eb4dac22822f740e
samskinnerphd <samskinnerphd@gmail.com>
2017-01-24 17:39:21 -0500

    add Scatter Plot & minor tweaks

--- app/controllers/residential_controller.rb
+++ app/controllers/residential_controller.rb
@@ -3,7 +3,7 @@ class ResidentialController < ApplicationController
 
   def data
     totals = SalesFigure.group(:jurisdiction).sum(:total_sales)
-    render :json => { :totals => totals }
+    render :json => { totals: totals }
   end
 
   def bar_chart; end
@@ -13,6 +13,15 @@ class ResidentialController < ApplicationController
     select(:id, :zipcode, :median_value).
       where(:jurisdiction => 'Baltimore').
       order('median_value DESC, zipcode')
-    render :json => { :bar_data => bar_data }
+    render :json => { bar_data: bar_data }
+  end
+
+  def scatter_plot; end
+
+  def scatter_data
+    data = SalesFigure.
+      select(:id, :zipcode, :jurisdiction, :median_value, :total_sales).
+      order(:jurisdiction)
+    render :json => {scatter_data: data}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'bar-chart' => 'residential#bar_chart'
   get 'residential/bar_data', :defaults => { format: 'json'}
 
+  get 'scatter-plot' => 'residential#scatter_plot'
+  get 'residential/scatter_data', :defaults => { format: 'json'}
+
   root 'residential#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
